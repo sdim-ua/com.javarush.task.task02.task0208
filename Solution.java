4011fc4e15e3791ca3dddb08b12199ff7522139d
sdim-ua <ingersol@yandex.ua>
2018-12-18 15:11:39 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+/* 
+Одного кота нам мало
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        
+    }
+
+    public static class Cat {
+
+    }
+}
